fa4a6f3ba0403d414e85334be492cbcec26e79ed
matthewcosgrove <mcosgrove@pivotal.io>
2016-06-19 12:03:40 +0100

    Renamed test

--- src/test/java/com/mc/PCFAutoscalerTestApplicationTests.java
+++ src/test/java/com/mc/PCFAutoscalerTestApplicationTests.java
@@ -1,6 +1,5 @@
 package com.mc;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
@@ -8,7 +7,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = PCFAutoscalerTestApplication.class)
-public class FibCpuTestApplicationTests {
+public class PCFAutoscalerTestApplicationTests {
 
 	@Test
 	public void contextLoads() {
